6bedb97f877f622f66144d33b24917ce6782405e
asimfarooq5 <asimfarooq5@gmail.com>
2025-04-08 11:54:33 +0000

    Update web artifacts from d61f2fa962e0ca918767f281e3e68ed9e6afc6a1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2711583302"
+    serviceWorkerVersion: "3799356796"
   }
 });
